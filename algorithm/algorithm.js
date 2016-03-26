21ff25fe7f23836f11dc0c80ffe07f884e666fc0
David Sudia <dsudia@gmail.com>
2016-03-26 16:04:25 -0600

    matchAge function works correctly

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -3,7 +3,9 @@ var teacher = require('./teacher1');
 var matchAge = require('./matchers/matchAge');
 
 function match (school, teacher) {
-  var ageMatchPer = matchAge(school.ages, teacher.ages);
+  var ageMatchPer = matchAge(school.ages, teacher.ages).toFixed(2);
+  console.log(ageMatchPer);
+  return ageMatchPer;
 }
 
-match();
+match(school, teacher);
