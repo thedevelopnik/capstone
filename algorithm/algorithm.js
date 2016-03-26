d4776b987d19e95f1d6906c96445d41d1853db45
David Sudia <dsudia@gmail.com>
2016-03-26 16:01:27 -0600

    matchAge function written

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,5 +1,9 @@
-function match (school, teacher) {
+var school = require('./school1');
+var teacher = require('./teacher1');
+var matchAge = require('./matchers/matchAge');
 
+function match (school, teacher) {
+  var ageMatchPer = matchAge(school.ages, teacher.ages);
 }
 
 match();
--- algorithm/matchers/matchAge.js
+++ algorithm/matchers/matchAge.js
@@ -1,3 +1,36 @@
-module.exports = function (schAge, teaAge) {
+// does this teacher teach any of the age bands at the school?
+function someMatch (schAge, teaAge) {
+  var someMatch = teaAge.some(function(el, ind, arr) {
+    for (i = 0; i < schAge.length; i++) {
+      return el === schAge[i];
+    }
+  });
+  return someMatch;
+}
 
+// how many?
+function countNumOfMatches (schAge, teaAge) {
+  var countOfMatches = 0;
+  teaAge.forEach(function(el, ind, arr) {
+    for (i = 0; i < schAge.length; i++) {
+      if (el === schAge[i]) {
+        countOfMatches += 1;
+      }
+    }
+  });
+  return countOfMatches;
+}
+
+// divide number of matches by number of school grade bands
+function findDecimal(divisor, dividend) {
+  return (divisor/dividend);
+}
+
+module.exports = function (schAge, teaAge) {
+  if (someMatch(schAge, teaAge) === true) {
+    var count = countNumOfMatches(schAge, teaAge);
+    return findDecimal(count, schAge.length);
+  } else {
+    return (-1);
+  }
 };
