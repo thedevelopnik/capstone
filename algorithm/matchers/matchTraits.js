d05d1f4a726f05bddc2dac7f2d3c6b64f0e5354b
David Sudia <dsudia@gmail.com>
2016-03-30 14:59:42 -0600

    matchTraits done

--- algorithm/matchers/matchTraits.js
+++ algorithm/matchers/matchTraits.js
@@ -1,3 +1,8 @@
 module.exports = function (schTraits, teaTraits) {
-
+  if (calc.someMatch(schAge, teaAge) === true) {
+    var count = calc.countNumOfMatches(schAge, teaAge);
+    return calc.findDecimal(count, schAge.length);
+  } else {
+    return (0);
+  }
 };
