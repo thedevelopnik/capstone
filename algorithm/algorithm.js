207cd191eff21bfa7630ca22916c92676afd9b70
David Sudia <dsudia@gmail.com>
2016-03-30 14:57:33 -0600

    removed a console.log

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -3,7 +3,7 @@ var teacher = require('./teacher1');
 var matchAge = require('./matchers/matchAge');
 
 function match (school, teacher) {
-  var ageMatchPer = matchAge(school.ages, teacher.ages).toFixed(2);
+  var ageMatchPer = matchAge(school.ages, teacher.ages).toFixed(2)
   return ageMatchPer;
 }
 
