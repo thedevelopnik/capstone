c7d5c3e63fa3bc3676f37931031cd17209105b02
David Sudia <dsudia@gmail.com>
2016-03-30 14:53:38 -0600

    matchCal and matchLoc functions done

--- algorithm/matchers/matchCal.js
+++ algorithm/matchers/matchCal.js
@@ -2,5 +2,5 @@ var calc = require('../calculators');
 
 module.exports = function (schCal, teaCal) {
   var count = calc.countNumOfMatches(schCal, teaCal);
-  return calc.findDecimal(count, schCal.length);
+  return calc.findDecimal(count, teaCal.length);
 };
--- algorithm/matchers/matchLoc.js
+++ algorithm/matchers/matchLoc.js
@@ -1,3 +1,9 @@
-module.exports = function (schLoc, teaLoc) {
+var calc = require('../calculators');
 
+module.exports = function (schLoc, teaLoc) {
+  if (calc.someMatch(schLoc, teaLoc) === true) {
+    return 1;
+  } else {
+    return (-1);
+  }
 };
